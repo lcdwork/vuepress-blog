2a6f1659d0722c91128482b322d289b8b18849fd
lcd <83559178@qq.com>
2021-07-01 12:39:02 +0800

    更改仓库名称

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
   title: "lcd's blog",
   description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
-  base: '/blog/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/vuepress-blog/', // 格式：'/<仓库名>/'， 默认'/'
   // dest: '../blog', //打包路径
   markdown: {
     lineNumbers: true, // 代码行号
